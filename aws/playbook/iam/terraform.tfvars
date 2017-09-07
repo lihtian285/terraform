fe9a1305a34cd7bbadb847f99d5cee2fb860774e
Lih Tian <lihtian@gmail.com>
2017-09-07 03:42:31 +0000

    New iam tf

---
+++ aws/playbook/iam/terraform.tfvars
@@ -0,0 +1,4 @@
+aws_access_key = "" 
+aws_secret_key = ""
+//aws_region = "ap-southeast-1"
+//ami_id = "ami-10bb2373"
---
+++ aws/playbook/iam/variables.tf
@@ -0,0 +1,14 @@
+//variable "instance_name" {
+//  description = "Used to populate the Name tag. This is done in main.tf"
+//}
+
+//variable "ami_id" {
+//  description = "The AMI to use"
+//}
+
+// Variables for providers used in this module
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+//variable "aws_region" {}
+
+//variable "instance_type" {}
